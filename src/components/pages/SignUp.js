ea139778f44ae9bd2752c9dc7d73c473af3ce1b5
romeo-alpha <82055038+romeo-alpha@users.noreply.github.com>
2021-05-23 17:34:28 +0530

    Update SignUp.js

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function SignUp() {
-  return <h1 className='signup'>SIGN UP FOR UPDATES AND SPECIAL OFFERS</h1>;
+  return <h1 className='sign-up'>SIGN UP FOR UPDATES AND SPECIAL OFFERS</h1>;
 }
